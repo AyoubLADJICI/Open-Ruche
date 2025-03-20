c7446bbdc70af6172873d2ec7b9d522804a2290f
votre_login <votre_mail>
2025-03-21 00:27:46 +0100

    Correction fonction poids

--- Test unitaire/LoraSendAndReceive/arduino_secrets.h
+++
@@ -1,3 +0,0 @@
-// Replace with keys obtained from TheThingsNetwork console
-#define SECRET_APP_EUI "213D57ED00000000"
-#define SECRET_APP_KEY "FA813BD835A67CBE3EF4298A06FAB916"
--- Test unitaire/Nano/nano/nano.ino
+++
@@ -1,8 +0,0 @@
-void setup() {
-    Serial1.begin(9600); // Communication série via Serial1
-}
-
-void loop() {
-    Serial1.println("Talla B-Nahl");
-    delay(1000);
-}
